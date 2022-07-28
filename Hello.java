68183786518867bf9b2b0f681894f9baf1b5f49a
mfirez <101753477+mfirez@users.noreply.github.com>
2022-07-28 20:34:09 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins github   yeni"); 
+        System.out.println("Hello Jenkins github   yeni 11"); 
     }
 }
